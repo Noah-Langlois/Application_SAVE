14d11e2c11b4a597420ebad07a341abb1c5d42db
Noah LANGLOIS <langloin@ip-192-168-196-106>
2024-01-12 11:33:18 +0100

    Factorisation du code en utilisant CSS et non JS pour la version mobile de la gestion des admins

--- frontend/client_vue_global/src/components/AdminManager.vue
+++ frontend/client_vue_global/src/components/AdminManager.vue
@@ -6,9 +6,6 @@ const router = useRouter()
 
 const store = inject('STORE')
 
-store.methods.setIsMobile()
-window.onresize = store.methods.setIsMobile
-
 function changeRoute(value) {
   console.log(store.system.debug)
   router.push({
@@ -20,160 +17,80 @@ function changeRoute(value) {
 
 <template>
     <main>
-      <!--Version Ordinateur-->
-      <div id="adminManagerDesktop" v-if="!store.state.isMobile">
+      <div>
         <div class="mt-5">
             <h1>Gestion des administrateurs</h1>
         </div>
         <div class="row mt-5">
-          <div class="col-6">
+          <div class="col-xl-6 mb-3">
             <div class="card">
               <div class="card-body">
-                <h5 class="card-title">Administrateurs</h5>
+                <h5 class="card-title text-truncate">Administrateurs</h5>
                 <h6 class="card-subtitle mb-2 text-body-secondary">Liste des administrateurs de SAVE</h6>
-                <ul class="mt-5">
+                <ul class="mt-5 text-truncate">
                   <li>SuperAdmin</li>
                 </ul>
               </div>
             </div>
           </div>
-          <div class="col-1">
-            <!--Bouttons d'ajout et suppression d'admins qui activent un modal-->
-            <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#ajoutModal">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
-                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z"/>
-              </svg> Ajouter         
-            </button>
-            <!-- Modal d'ajout admin -->
-            <div class="modal fade" id="ajoutModal" tabindex="-1" aria-labelledby="ajoutModalLabel" aria-hidden="true">
-              <div class="modal-dialog">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="ajoutModalLabel">Ajouter un administrateur</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                  </div>
-                  <div class="modal-body">
-                    <h4>Pseudo :</h4>
-                    <input class="form-control"/>
-                  </div>
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
-                    <button type="button" class="btn btn-primary">Valider</button>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <!--Boutton de supression-->
-            <button class="btn btn-danger mt-2" data-bs-toggle="modal" data-bs-target="#suppModal">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-circle-fill" viewBox="0 0 16 16">
-                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1z"/>
-              </svg> Supprimer
-            </button>
-            <!-- Modal de supressions admin -->
-            <div class="modal fade" id="suppModal" tabindex="-1" aria-labelledby="suppModalLabel" aria-hidden="true">
-              <div class="modal-dialog">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="suppModalLabel">Supprimer un administrateur</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                  </div>
-                  <div class="modal-body">
-                    <h4>Pseudo :</h4>
-                    <input class="form-control"/>
-                  </div>
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
-                    <button type="button" class="btn btn-primary">Valider</button>
+          <div class="col ms-2">
+            <div class="row">
+              <!--Bouttons d'ajout et suppression d'admins qui activent un modal-->
+              <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#ajoutModal" style="max-width: 10rem;">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+                  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z"/>
+                </svg> Ajouter         
+              </button>
+              <!-- Modal d'ajout admin -->
+              <div class="modal fade" id="ajoutModal" tabindex="-1" aria-labelledby="ajoutModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h1 class="modal-title fs-5" id="ajoutModalLabel">Ajouter un administrateur</h1>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                      <h4>Pseudo :</h4>
+                      <input class="form-control"/>
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
+                      <button type="button" class="btn btn-primary">Valider</button>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-        <div class="mt-5">
-            <button class="btn btn-primary" @click="changeRoute('HomeAdmin')">Retour</button>
-        </div>
-      </div>
-
-      <!--Version Mobile-->
-      <div id="adminManagerMobile" v-else>
-        <div class="row justify-content-center">
-          <div class="col container text-center">
-            <h1>Gestion des administrateurs</h1>
-          </div>
-        </div>
-        <div class="row justify-content-center">
-          <div class="col d-flex justify-content-center">
-            <div class="card">
-              <div class="card-body">
-                <h5 class="card-title">Administrateurs</h5>
-                <h6 class="card-subtitle mb-2 text-body-secondary">Liste des administrateurs de SAVE</h6>
-                <ul class="mt-5">
-                  <li>SuperAdmin</li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="row justify-content-center mt-2">
-          <div class="col container text-center">
-            <!--Boutton d'ajout d'un admin-->
-            <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#ajoutModal" style="max-width: 18rem;;">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
-                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z"/>
-              </svg> Ajouter         
-            </button>
-            <!-- Modal d'ajout admin -->
-            <div class="modal fade" id="ajoutModal" tabindex="-1" aria-labelledby="ajoutModalLabel" aria-hidden="true">
-              <div class="modal-dialog">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="ajoutModalLabel">Ajouter un administrateur</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                  </div>
-                  <div class="modal-body">
-                    <h4>Pseudo :</h4>
-                    <input class="form-control"/>
-                  </div>
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
-                    <button type="button" class="btn btn-primary">Valider</button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div> 
-          <!--Boutton de supression-->
-        </div>
-        <div class="row justify-content-center">
-          <div class="col container text-center">
-            <button class="btn btn-danger mt-2" data-bs-toggle="modal" data-bs-target="#suppModal">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-circle-fill" viewBox="0 0 16 16">
-                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1z"/>
-              </svg> Supprimer
-            </button>
-            <!-- Modal de supressions admin -->
-            <div class="modal fade" id="suppModal" tabindex="-1" aria-labelledby="suppModalLabel" aria-hidden="true">
-              <div class="modal-dialog">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="suppModalLabel">Supprimer un administrateur</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                  </div>
-                  <div class="modal-body">
-                    <h4>Pseudo :</h4>
-                    <input class="form-control"/>
-                  </div>
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
-                    <button type="button" class="btn btn-primary">Valider</button>
+            <div class="row mb-5">
+              <!--Boutton de supression-->
+              <button class="btn btn-danger mt-2" data-bs-toggle="modal" data-bs-target="#suppModal" style="max-width: 10rem;">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-circle-fill" viewBox="0 0 16 16">
+                  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1z"/>
+                </svg> Supprimer
+              </button>
+              <!-- Modal de supressions admin -->
+              <div class="modal fade" id="suppModal" tabindex="-1" aria-labelledby="suppModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h1 class="modal-title fs-5" id="suppModalLabel">Supprimer un administrateur</h1>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                      <h4>Pseudo :</h4>
+                      <input class="form-control"/>
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
+                      <button type="button" class="btn btn-primary">Valider</button>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div class="mt-5">
+        <div>
             <button class="btn btn-primary" @click="changeRoute('HomeAdmin')">Retour</button>
         </div>
       </div>
